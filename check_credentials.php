96feb81380381e259041ea1e01e218d39bb82494
alin <herciualin10@gmail.com>
2024-10-24 12:29:39 +0200

    Update check_credentials.php

--- check_credentials.php
+++ check_credentials.php
@@ -6,15 +6,17 @@ function check_credentials($email, $password)
     
     $hashedPassword = hash_password($password);
 
-    $querry = "SELECT email, mot_de_passe FROM users where email = {$email} AND mot_de_passe = {$hashedPassword}";
-
-    $result = $conn->query($querry);
+    $stmt = $conn->prepare("SELECT email, mot_de_passe FROM users where email = ? AND mot_de_passe = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->bind_param("s", $hashedPassword);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows == 1) {
         return true;
     } else {
         return false;
     } 
-    
+
 }
 ?>
