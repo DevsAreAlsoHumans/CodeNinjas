6e7146b537bcccbe5c09d56da055eddc93defd66
Theo Mousnier <mousniert.pro@gmail.com>
2024-10-24 14:56:43 +0200

    update

--- addFormulaire.php
+++ addFormulaire.php
@@ -2,30 +2,64 @@
 <html lang="fr">
 <body>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>formulate</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet"
+          type="text/css"
+          href="style.css" />
+    <title>S’inscrire sur notre site</title>
 </head>
-<?php
-try {
-    global$conn;
-    require "db.php";
-    $sql = "INSERT INTO users (nom, prenom,email ,date_naissance, password) VALUES (:nom, :prenom, :email,:date_naissance, :password)";
-    $requete = $conn->prepare($sql);
-    $requete->bindValue(':nom', $_POST['nom']);
-    $requete->bindValue(':prenom', $_POST['prenom']);
-    $requete->bindValue(':email', $_POST['email']);
-    $requete->bindValue(':date_naissance', $_POST['date_naissance']);
-    $requete->bindValue(':password', $_POST['password']);
-    if ($requete->execute()) {
-        print("Votre compte a bien été crée");
-    } else {
-        print("Erreur lors de la création du compte");
-    }
-} catch (PDOException $e) {
-    print("Erreur de connexion à la base de données: " . $e->getMessage());
+<form method="post" action="">
+<div class="container mt-5">
+    <div class="row justify-content-md-center">
+        <div class="col-5" >
+            <h2>S’inscrire</h2><br><br>
+            <div class="form-group mb-3">
+                <input type="name" name="nom" id="nom" placeholder="Nom" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+                <input type="name" name="prenom" id="prenom" placeholder="Prénom" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+                <input type="date" name="date_naissance" id="date_naissance" placeholder="Date de naissance" class="form-control">
+            </div>
 
+            <div class="form-group mb-3">
+                <input type="email" name="email" id="email" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+                <input type="password" name="password" id="password" placeholder="Password" class="form-control">
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-dark">Inscription</button>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
+
+<?php
+require "db.inc.php";
+if (isset($_POST['nom']) && $_POST['nom'] != '' && isset($_POST['prenom']) && $_POST['prenom'] != '' &&isset($_POST['email']) && $_POST['email'] != '' && isset($_POST['date_naissance']) && $_POST['date_naissance'] != '' && isset($_POST['password']) && $_POST['password'] != '') {
+    try {
+        $sql = "INSERT INTO users (nom, prenom,email ,date_naissance, password) VALUES (:nom, :prenom, :email,:date_naissance, :password)";
+        if (isset($conn)) {
+            $requete = $conn->prepare($sql);
+            $requete->bindValue(':nom', $_POST['nom']);
+            $requete->bindValue(':prenom', $_POST['prenom']);
+            $requete->bindValue(':email', $_POST['email']);
+            $requete->bindValue(':date_naissance', $_POST['date_naissance']);
+            $requete->bindValue(':password', $_POST['password']);
+            if ($requete->execute()) {
+                print("Your account has been created");
+            } else {
+                print("Account creation error");
+            }
+        }
+    } catch (PDOException $e) {
+        print("Database connection error: " . $e->getMessage());
+    }
 }
 ?>
 </body>
--- db.inc.php
+++ db.inc.php
@@ -5,10 +5,8 @@ $password = "";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=code_ninjas", $username, $password);
-  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
