6d208aac69a8eed5ef156684b98522d71c6b3c82
stradimarius <mariusclement68@gmail.com>
2024-10-24 12:27:59 +0200

    verify_email

--- validationServeur/user_info.php
+++ validationServeur/user_info.php
@@ -1,8 +1,24 @@
 <?php
-
+require __DIR__. '/db.php';
 function is_valid($email)
 {
     return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+}
+
+function mail_exist($email, $db, $tablename) {
+$sql = "SELECT * FROM ". $tablename. "WHERE email='". $email."'";
+$results = $db->query($sql);
+$row = $results->fetch_assoc();
+
+return (is_array($row) && count($row)>0);
+
+if(!is_valid($email)):
+    $result['has_error'] = 1;
+    $result['response'] = "Email address is invalid.";
+elseif(mail_exist($db, "employees", $email)):
+    $result['has_error'] = 1;
+    $result['response'] = "Email address is already exists.";
+endif;
 }
 
 ?>
