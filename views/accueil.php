3152517c6bb81988b6677da87d5b5accf3511695
TheSpaceRangers <97059309+TheSpaceRangers@users.noreply.github.com>
2024-10-24 16:30:35 +0200

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,12 +1,14 @@
+<?php include('header.php'); ?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head <?php include('header.php'); ?>>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <h1>Bienvenue</h1>
-</head>
-<body>
-    
-</body>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil</title>
+        <h1>Bienvenue</h1>
+    </head>
+        
+    <body>
+    </body>
 </html>
