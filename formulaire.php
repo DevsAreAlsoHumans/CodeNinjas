cb01029e69453059bc75011ef958cd990516a362
Theo Mousnier <mousniert.pro@gmail.com>
2024-10-24 12:52:28 +0200

    update

--- formulaire.php
+++ formulaire.php
@@ -12,9 +12,9 @@ try {
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $conn = new PDO("mysql:host=$servername;dbname=code_ninjas", $username, $password);
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=code_ninjas", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
