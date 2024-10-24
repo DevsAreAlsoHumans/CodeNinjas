4c2c1995329a09f5652308ee7f308e7b0312a8c3
Theo Mousnier <mousniert.pro@gmail.com>
2024-10-24 11:42:14 +0200

    create formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulate</title>
+</head>
+<?php
+global$conn;
+require 'config.inc.php';
+if (isset($_POST['nom']) && $_POST['nom'] != '' && isset($_POST['prenom']) && $_POST['prenom'] != '' && isset($_POST['email']) && $_POST['email'] != '' && isset($_POST['password']) && $_POST['password'] != '') {
+    try {
+        $db = new PDO($conn);
+        $sql = "INSERT INTO users (nom, prenom, email, password) VALUES (:nom, :pouvoir, :email, :password)";
+        $requete = $db->prepare($sql);
+        $requete->bindValue(':nom', $_POST['nom']);
+        $requete->bindValue(':prenom', $_POST['prenom']);
+        $requete->bindValue(':prenom', $_POST['prenom']);
+        $requete->bindValue(':email', $_POST['email']);
+        $requete->bindValue(':password', $_POST['password']);
+        if ($requete->execute()) {
+            print("Votre compte a bien été crée");
+        } else {
+            print("Erreur lors de la création du compte");
+        }
+    } catch (PDOException $e) {
+        print("Erreur de connexion à la base de données: " . $e->getMessage());
+    }
+}
+?>
+</body>
+</html>
